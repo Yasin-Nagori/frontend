8e8eb52b0d37402262cd4e1bc7bf26f308361752
Yasin-Nagori <ynagori860@gmail.com>
2024-07-03 16:17:07 +0530

    change

---
+++ firebase.init.js
@@ -0,0 +1,24 @@
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC0QVSRnyPOU09Kzg8FTEIaMz2RkJ8MtvE",
+  authDomain: "twitter-clone-5e96d.firebaseapp.com",
+  projectId: "twitter-clone-5e96d",
+  storageBucket: "twitter-clone-5e96d.appspot.com",
+  messagingSenderId: "519166971554",
+  appId: "1:519166971554:web:8334b9e28035fd1f4051fb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+export default auth;
+
+// Initialize Cloud Firestore and get a reference to the service
