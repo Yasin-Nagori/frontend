11de6afd071f301810e0db40ff7889b08738ac6e
Yasin-Nagori <ynagori860@gmail.com>
2024-07-09 16:08:09 +0530

    change

--- firebase.iinits.js
+++ firebase.iinits.js
@@ -9,8 +9,8 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
+  apiKey: "AIzaSyC0QVSRnyPOU09Kzg8FTEIaMz2RkJ8MtvE",
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  apiKey: import.meta.env.VITE_APIKEY,
   projectId: import.meta.env.VITE_PROJECTID,
   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
