cf5f9d4f2ef04a6b987205d2854ff60fb9168977
Yasin-Nagori <ynagori860@gmail.com>
2024-07-12 15:31:54 +0530

    change

--- firebase.iinits.js
+++ firebase.iinits.js
@@ -1,5 +1,7 @@
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -18,6 +20,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getFirestore(app);
 
-export { auth, db };
+export default auth;
